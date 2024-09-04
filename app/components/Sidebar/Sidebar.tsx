82d53a08dc4320f098710ff865ae8505155bb082
Daniel Martins <martinsda@gmail.com>
2024-09-04 18:08:42 -0300

    edit icons

--- app/components/Sidebar/Sidebar.tsx
+++ app/components/Sidebar/Sidebar.tsx
@@ -1,35 +1,35 @@
 import SidebarMenuItem from "./SidebarMenuItem";
 
-import FileCheck from "../../assets/icons/FileCheck.svg";
-import Wallet from "../../assets/icons/Wallet.svg";
-import BadgeCheck from "../../assets/icons/BadgeCheck.svg";
 import SeedPhraseImage from "../../assets/icons/Image.svg";
-import PiggyBank from "../../assets/icons/PiggyBank.svg";
-import FileDigit from "../../assets/icons/FileDigit.svg";
+import EditDocument from "../../assets/icons/EditDocument.svg";
+import VerifiedUser from "../../assets/icons/VerifiedUser.svg";
+import Savings from "../../assets/icons/Savings.svg";
+import AccountBalanceWallet from "../../assets/icons/AccountBalanceWallet.svg";
+import SwapHoriz from "../../assets/icons/SwapHoriz.svg";
 
 const Sidebar = () => {
   return (
     <div className="w-64 h-full flex flex-col gap-y-2 py-4 px-4 items-start bg-gray-100">
       <SidebarMenuItem
         label="Notarization"
-        icon={FileCheck}
+        icon={EditDocument}
         src="/notarization"
       />
       <SidebarMenuItem
         label="Signature verifier"
-        icon={BadgeCheck}
+        icon={VerifiedUser}
         src="/signature-verifier"
       />
       <SidebarMenuItem
         label="Proof of funds"
-        icon={PiggyBank}
+        icon={Savings}
         src="/proof-of-funds"
       />
-      <SidebarMenuItem label="Address monitoring" icon={Wallet} />
+      <SidebarMenuItem label="Address monitoring" icon={AccountBalanceWallet} />
       <SidebarMenuItem label="Seed phase into image" icon={SeedPhraseImage} />
       <SidebarMenuItem
         label="BIP39 hexadecimal to word converter"
-        icon={FileDigit}
+        icon={SwapHoriz}
         src="/bip39-converter"
       />
     </div>
