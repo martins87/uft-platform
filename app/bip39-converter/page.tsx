85777cf9ec390394ffec22698b11c9df44b9ab96
Daniel Martins <martinsda@gmail.com>
2024-09-11 23:54:43 -0300

    add animation to icon when value copied

--- app/bip39-converter/page.tsx
+++ app/bip39-converter/page.tsx
@@ -3,14 +3,15 @@
 import { useState } from "react";
 import Image from "next/image";
 import converter from "hex2dec";
+import { twMerge } from "tailwind-merge";
 
 import Container from "../components/Container";
 import Header from "../components/Header";
 import Card from "../components/Card";
 import ArrowForward from "../assets/icons/ArrowForward.svg";
 import Copy from "../assets/icons/Copy.svg";
+import Check from "../assets/icons/Check.svg";
 import { bip39Words } from "../data/bip0039-english.js";
-import { twMerge } from "tailwind-merge";
 
 const words = ["Taxation is theft", ...bip39Words];
 
@@ -19,6 +20,7 @@ const BIP39Converter = () => {
   const [bip39Word, setBip39Word] = useState<string>("pizza");
   const [decimalWord, setDecimalWord] = useState<string>("");
   const [decimal, setDecimal] = useState<string>("");
+  const [icons, setIcons] = useState<any[]>([Copy, Copy]);
 
   const getWordFromHex = (hexStr: string) => {
     let index: number = Number(converter.hexToDec(hexStr));
@@ -60,7 +62,19 @@ const BIP39Converter = () => {
     }
   };
 
-  const handleCopy = (value: string) => navigator.clipboard.writeText(value);
+  const handleCopy = (value: string, index: number) => {
+    navigator.clipboard.writeText(value);
+
+    if (index === 0) {
+      setIcons([Check, Copy]);
+    } else {
+      setIcons([Copy, Check]);
+    }
+
+    setTimeout(() => {
+      setIcons([Copy, Copy]);
+    }, 1500);
+  };
 
   return (
     <Container>
@@ -91,9 +105,9 @@ const BIP39Converter = () => {
               "absolute right-2 top-[50%] -translate-y-1/2",
               hexadecimalWord ? "hover:cursor-pointer" : "opacity-50"
             )}
-            src={Copy}
+            src={icons[0]}
             alt="Copy to clipboard"
-            onClick={() => handleCopy(bip39Word)}
+            onClick={() => handleCopy(bip39Word, 0)}
           />
         </div>
 
@@ -119,9 +133,9 @@ const BIP39Converter = () => {
               "absolute right-2 top-[50%] -translate-y-1/2",
               decimalWord ? "hover:cursor-pointer" : "opacity-50"
             )}
-            src={Copy}
+            src={icons[1]}
             alt="Copy to clipboard"
-            onClick={() => handleCopy(bip39Word)}
+            onClick={() => handleCopy(decimal, 1)}
           />
         </div>
       </Card>
