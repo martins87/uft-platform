4f712489815ab727d36a8ffa0a1a1856e3464c6f
Daniel Martins <martinsda@gmail.com>
2024-10-14 16:16:00 -0300

    edit multisig address creator styling

--- app/multisig-address-creator/page.tsx
+++ app/multisig-address-creator/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { useState } from "react";
+import Image from "next/image";
+import { twMerge } from "tailwind-merge";
 
 import Container from "../components/Container";
 import Header from "../components/Header";
@@ -9,18 +11,20 @@ import Button from "../components/Button";
 import PubKey from "./PubKey";
 import Plus from "../assets/icons/Plus.svg";
 import { createMultisigAddress } from "../lib/bitcoin";
-import { twMerge } from "tailwind-merge";
 import DropdownMenuCheckboxes from "./Dropbox";
+import Copy from "../assets/icons/Copy.svg";
+import Check from "../assets/icons/Check.svg";
 
 const MultisigAddressCreator = () => {
   const [signatures, setSignatures] = useState<number>(1);
   const [pubKeys, setPubKeys] = useState<string[]>([""]);
   const [multisigAddress, setMultisigAddress] = useState<string>("?");
+  const [icons, setIcons] = useState<any[]>([Copy, Copy]);
 
   const handleAddPubKey = () => setPubKeys((state: string[]) => [...state, ""]);
 
   const truncateAddress = (hash: string) => {
-    return hash.substring(0, 7) + "..." + hash.slice(-7);
+    return hash.substring(0, 10) + "..." + hash.slice(-10);
   };
 
   const handleCreateAddress = () => {
@@ -31,6 +35,16 @@ const MultisigAddressCreator = () => {
     } catch (error) {
       console.error("Error on creating the multisig address", error);
     }
+  };
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText(multisigAddress);
+
+    setIcons([Check, Copy]);
+
+    setTimeout(() => {
+      setIcons([Copy, Copy]);
+    }, 1500);
   };
 
   return (
@@ -57,7 +71,7 @@ const MultisigAddressCreator = () => {
         ))}
         <div className="flex flex-col sm:flex-row items-center gap-2">
           <Button
-            className="gap-2 w-full mobile:w-fit"
+            className="gap-2 w-full sm:w-fit"
             primary
             onClick={handleAddPubKey}
             label="Add pubkey"
@@ -65,7 +79,7 @@ const MultisigAddressCreator = () => {
             iconSide="left"
           />
           <Button
-            className="w-full mobile:w-fit"
+            className="w-full sm:w-fit"
             primary
             onClick={handleCreateAddress}
             label="Create address"
@@ -73,13 +87,29 @@ const MultisigAddressCreator = () => {
         </div>
         <div
           className={twMerge(
-            "flex items-center gap-2 mx-auto mt-4",
+            "flex items-center gap-2 mt-4",
             multisigAddress.length > 1 ? "flex-col" : ""
           )}
         >
           <span>{`${signatures}-of-${pubKeys.length} multisig address:`}</span>
-          <span className="w-fit min-h-12 flex items-center gap-2 bg-orange-100 border border-orange-300 text-lg px-4 py-2 rounded-xl text-center">
-            {multisigAddress}
+          <span className="relative w-full sm:w-fit md:w-full tablet:w-fit min-h-12 items-center gap-2 bg-orange-100 border border-orange-300 text-lg pl-4 pr-12 py-2 rounded-xl">
+            <div className="mobile:hidden md:flex tablet:hidden">
+              {truncateAddress(multisigAddress)}
+            </div>
+            <div className="hidden mobile:flex md:hidden tablet:flex">
+              {multisigAddress}
+            </div>
+            <Image
+              className={twMerge(
+                "absolute right-2 top-[50%] -translate-y-1/2",
+                multisigAddress.length > 1
+                  ? "hover:cursor-pointer"
+                  : "opacity-50"
+              )}
+              src={icons[0]}
+              alt="Copy to clipboard"
+              onClick={() => handleCopy()}
+            />
           </span>
         </div>
       </Card>
