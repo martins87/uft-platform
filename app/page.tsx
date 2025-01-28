91dc9e3c4113bffa210e5a2ebc484adc465b7440
Daniel Martins <martinsda@gmail.com>
2025-01-28 07:16:43 -0300

    add redirect to main domain

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,12 @@
+"use client";
+
+import { useEffect } from "react";
+import { redirect } from "next/navigation";
+
 export default function Home() {
+  useEffect(() => {
+    redirect("https://www.operationblockchain.app/");
+  }, []);
+
   return <p>Hello there</p>;
 }
